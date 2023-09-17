41c117ebe920af0b89eed2dff76db0279046c88e
Priyanshu-Butterpaper1 <priyanshu.mishra@butterpaper.tech>
2023-09-17 16:50:10 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -6,15 +6,16 @@ var cors = require("cors");
 dotenv.config();
 connectTOMongo();
 
-const corsOptions = {
-  origin: "https://airarticle.vercel.app",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-};
+// const corsOptions = {
+//   origin: "https://airarticle.vercel.app",
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+// };
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
